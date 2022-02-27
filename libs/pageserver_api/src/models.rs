850c524088c80c6273ac309482ce68a50afd8d86
Cuong Nguyen <ctring23@gmail.com>
2022-02-27 13:46:43 -0500

    Multi-region mode in zenith
    + Extend postgres <=> pageserver API to read from different timelines
    + Extend control plane cli to start pageserver in multi-region mode

--- libs/pageserver_api/src/models.rs
+++ libs/pageserver_api/src/models.rs
@@ -253,6 +253,7 @@ pub struct PagestreamExistsRequest {
     pub latest: bool,
     pub lsn: Lsn,
     pub rel: RelTag,
+    pub region: u32,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -260,6 +261,7 @@ pub struct PagestreamNblocksRequest {
     pub latest: bool,
     pub lsn: Lsn,
     pub rel: RelTag,
+    pub region: u32,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -268,6 +270,7 @@ pub struct PagestreamGetPageRequest {
     pub lsn: Lsn,
     pub rel: RelTag,
     pub blkno: u32,
+    pub region: u32,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -367,6 +370,7 @@ impl PagestreamFeMessage {
                     relnode: body.read_u32::<BigEndian>()?,
                     forknum: body.read_u8()?,
                 },
+                region: body.read_u32::<BigEndian>()?,
             })),
             1 => Ok(PagestreamFeMessage::Nblocks(PagestreamNblocksRequest {
                 latest: body.read_u8()? != 0,
@@ -377,6 +381,7 @@ impl PagestreamFeMessage {
                     relnode: body.read_u32::<BigEndian>()?,
                     forknum: body.read_u8()?,
                 },
+                region: body.read_u32::<BigEndian>()?,
             })),
             2 => Ok(PagestreamFeMessage::GetPage(PagestreamGetPageRequest {
                 latest: body.read_u8()? != 0,
@@ -388,6 +393,7 @@ impl PagestreamFeMessage {
                     forknum: body.read_u8()?,
                 },
                 blkno: body.read_u32::<BigEndian>()?,
+                region: body.read_u32::<BigEndian>()?,
             })),
             3 => Ok(PagestreamFeMessage::DbSize(PagestreamDbSizeRequest {
                 latest: body.read_u8()? != 0,
@@ -453,6 +459,7 @@ mod tests {
                     dbnode: 3,
                     relnode: 4,
                 },
+                region: 0,
             }),
             PagestreamFeMessage::Nblocks(PagestreamNblocksRequest {
                 latest: false,
@@ -463,6 +470,7 @@ mod tests {
                     dbnode: 3,
                     relnode: 4,
                 },
+                region: 0,
             }),
             PagestreamFeMessage::GetPage(PagestreamGetPageRequest {
                 latest: true,
@@ -474,6 +482,7 @@ mod tests {
                     relnode: 4,
                 },
                 blkno: 7,
+                region: 0,
             }),
             PagestreamFeMessage::DbSize(PagestreamDbSizeRequest {
                 latest: true,
