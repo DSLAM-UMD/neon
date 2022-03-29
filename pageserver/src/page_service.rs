a27500d161ddd73840bcfce86f48730e21e6cb86
Cuong Nguyen <ctring23@gmail.com>
2022-03-28 23:11:26 -0400

    Return LSN from zenith

--- pageserver/src/page_service.rs
+++ pageserver/src/page_service.rs
@@ -698,6 +698,7 @@ impl PageServerHandler {
             .await?;
 
         Ok(PagestreamBeMessage::Exists(PagestreamExistsResponse {
+            lsn,
             exists,
         }))
     }
@@ -717,6 +718,7 @@ impl PageServerHandler {
         let n_blocks = timeline.get_rel_size(req.rel, lsn, req.latest, ctx).await?;
 
         Ok(PagestreamBeMessage::Nblocks(PagestreamNblocksResponse {
+            lsn,
             n_blocks,
         }))
     }
@@ -768,6 +770,7 @@ impl PageServerHandler {
             .await?;
 
         Ok(PagestreamBeMessage::GetPage(PagestreamGetPageResponse {
+            lsn,
             page,
         }))
     }
@@ -812,7 +815,11 @@ impl PageServerHandler {
         }
 
         Ok(PagestreamBeMessage::GetSlruPage(
-            PagestreamGetSlruPageResponse { seg_exists, page },
+            PagestreamGetSlruPageResponse {
+                lsn,
+                seg_exists,
+                page,
+            },
         ))
     }
 
