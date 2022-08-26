05b4a80310995aeaef8ce40870cabacb2fd4a051
Cuong Nguyen <ctring23@gmail.com>
2022-08-26 15:29:56 -0400

    Make neon aware of pg_csn

--- libs/pageserver_api/src/reltag.rs
+++ libs/pageserver_api/src/reltag.rs
@@ -139,6 +139,7 @@ pub enum SlruKind {
     Clog,
     MultiXactMembers,
     MultiXactOffsets,
+    Csn,
 }
 
 impl SlruKind {
@@ -147,6 +148,7 @@ impl SlruKind {
             Self::Clog => "pg_xact",
             Self::MultiXactMembers => "pg_multixact/members",
             Self::MultiXactOffsets => "pg_multixact/offsets",
+            Self::Csn => "pg_csn",
         }
     }
 }
