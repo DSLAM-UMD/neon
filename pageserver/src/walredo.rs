b7ad4d68aca02100abe5b4ba9f89aae6f8cf4c47
Pooja Nilangekar <poojan@umd.edu>
2022-09-16 18:39:34 -0400

    Address review comments

--- pageserver/src/walredo.rs
+++ pageserver/src/walredo.rs
@@ -54,7 +54,6 @@ use postgres_ffi::v14::nonrelfile_utils::{
     transaction_id_set_status, transaction_id_set_csn,
 };
 use postgres_ffi::BLCKSZ;
-use postgres_ffi::pg_constants::AbortedXidCSN;
 
 ///
 /// `RelTag` + block number (`blknum`) gives us a unique id of the page in the cluster.
@@ -566,8 +565,6 @@ impl PostgresRedoManager {
                     );
                     transaction_id_set_csn(xid, *lsn, page);
                 }
-
-
             }
             NeonWalRecord::CsnLogSetAborted { xids } => {
                 let (slru_kind, segno, blknum) =
@@ -598,7 +595,7 @@ impl PostgresRedoManager {
                         key
                     );
 
-                    transaction_id_set_csn(xid, AbortedXidCSN, page);
+                    transaction_id_set_csn(xid, pg_constants::AbortedXidCSN, page);
                 }
             }
         }
