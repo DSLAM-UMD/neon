ea5c57a3395562422e4f85d807fcdf869e915465
Cuong Nguyen <ctring23@gmail.com>
2022-10-28 21:44:11 -0400

    Turn on fetching csn log from neon

--- control_plane/src/endpoint.rs
+++ control_plane/src/endpoint.rs
@@ -378,9 +378,12 @@ impl Endpoint {
             // testing.
             conf.append("synchronous_standby_names", "pageserver");
         }
-
-        // UMD: Remotexact and multi-region configurations
+        
+        //
+        // Remotexact: multi-region configurations
+        //
         conf.append("enable_csn_snapshot", "on");
+        conf.append("neon.slru_csnlog", "on");
         // TODO(ctring): consider a different value or make this an argument somewhere
         conf.append("max_prepared_transactions", "64");
         conf.append(
