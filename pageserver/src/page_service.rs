e55c32ace44de81cb3c8d77b51fe245a2f64e5a2
Cuong Nguyen <ctring23@gmail.com>
2022-11-07 11:54:41 -0500

    Disable caching relsize for remote relations

--- pageserver/src/page_service.rs
+++ pageserver/src/page_service.rs
@@ -585,7 +585,7 @@ impl PageServerHandler {
         Ok(lsn)
     }
 
-    #[instrument(skip(self, timeline, req), fields(rel = %req.rel, req_lsn = %req.lsn))]
+    #[instrument(skip(self, timeline, req), fields(region = %timeline.region_id, rel = %req.rel, req_lsn = %req.lsn))]
     async fn handle_get_rel_exists_request(
         &self,
         timeline: &Timeline,
@@ -603,7 +603,7 @@ impl PageServerHandler {
         }))
     }
 
-    #[instrument(skip(self, timeline, req), fields(rel = %req.rel, req_lsn = %req.lsn))]
+    #[instrument(skip(self, timeline, req), fields(region = %timeline.region_id, rel = %req.rel, req_lsn = %req.lsn))]
     async fn handle_get_nblocks_request(
         &self,
         timeline: &Timeline,
@@ -621,7 +621,7 @@ impl PageServerHandler {
         }))
     }
 
-    #[instrument(skip(self, timeline, req), fields(dbnode = %req.dbnode, req_lsn = %req.lsn))]
+    #[instrument(skip(self, timeline, req), fields(region = %timeline.region_id, dbnode = %req.dbnode, req_lsn = %req.lsn))]
     async fn handle_db_size_request(
         &self,
         timeline: &Timeline,
@@ -642,7 +642,7 @@ impl PageServerHandler {
         }))
     }
 
-    #[instrument(skip(self, timeline, req), fields(rel = %req.rel, blkno = %req.blkno, req_lsn = %req.lsn))]
+    #[instrument(skip(self, timeline, req), fields(region = %timeline.region_id, rel = %req.rel, blkno = %req.blkno, req_lsn = %req.lsn))]
     async fn handle_get_page_at_lsn_request(
         &self,
         timeline: &Timeline,
@@ -670,7 +670,7 @@ impl PageServerHandler {
         }))
     }
 
-    #[instrument(skip(self, timeline, req), fields(slru_kind = %req.kind.to_str(), segno = %req.segno,
+    #[instrument(skip(self, timeline, req), fields(region = %timeline.region_id, slru_kind = %req.kind.to_str(), segno = %req.segno,
                  check_blkno = %req.blkno, req_lsn = %req.lsn, check_exists_only = %req.check_exists_only))]
     async fn handle_get_slru_page_at_lsn_request(
         &self,
